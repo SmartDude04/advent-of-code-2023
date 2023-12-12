fff51de1476498baacbaa4b61500fe099a06597b
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2023-12-12 11:16:56 -0500

    Finished day 11

--- day-11/day-11-2.py
+++ day-11/day-11-2.py
@@ -1,6 +1,6 @@
 # Use list comprehension to open the file and parse through
 lines = [line.strip() for line in open("day-11/day-11.txt", "r")]
-NUM_EXPANSIONS = 100 - 1
+NUM_EXPANSIONS = 1000000 - 1
 def sign(number: int) -> int:
     '''Returns 1 if the number is positive, -1 if negative, and 0 if 0'''
     if number > 0:
@@ -10,44 +10,53 @@ def sign(number: int) -> int:
     else:
         return 0
 
-def calculate_next_coord(current_coords:list[int], destination_coords:list[int]) -> list[int]:
-    '''Sees which distance to the coordanite is longer and decides to go that way.
-    This will return the value that will lead to the shortest path to the next galaxy'''
+def calculate_distance(expansions: list[list[int]], current_coords: list[int], destination_coords: list[int]) -> int:
+    '''Returns the distance to the two galaxies'''
     
-    distance_in_x = destination_coords[0] - current_coords[0]
-    distance_in_y = destination_coords[1] - current_coords[1]
+    distance_in_x = abs(destination_coords[0] - current_coords[0])
+    distance_in_y = abs(destination_coords[1] - current_coords[1])
     
-    if abs(distance_in_x) > abs(distance_in_y):
-        return [current_coords[0] + sign(distance_in_x), current_coords[1]]
-    else:
-        return [current_coords[0], current_coords[1] + sign(distance_in_y)]
-
-def add_extra_rows(array: list[str], row_num: int) -> list[str]:
-    '''Will add an extra row above the specified row'''
+    # Calculate expansions for x coords
+    for expansion in expansions[0]:
+        if current_coords[0] < expansion and destination_coords[0] > expansion:
+            distance_in_x += NUM_EXPANSIONS
+            
+        if current_coords[0] > expansion and destination_coords[0] < expansion:
+            distance_in_x += NUM_EXPANSIONS
     
-    extra_row = '.' * len(array[0])
-    
-    for i in range(NUM_EXPANSIONS):
-        array.insert(row_num, extra_row)
+    # Calculate expansions for y coords
+    for expansion in expansions[1]:
+        if current_coords[1] < expansion and destination_coords[1] > expansion:
+            distance_in_y += NUM_EXPANSIONS
         
-    return array
+        if current_coords[1] > expansion and destination_coords[1] < expansion:
+            distance_in_y += NUM_EXPANSIONS
     
-def add_extra_columns(array: list[str], column_num: int) -> list[str]:
-    '''Adds an extra comumn after the specified column'''
-    
-    return_arr = []
-    
-    for line in array:
-        return_arr.append(line[:column_num + 1] + ('.' * NUM_EXPANSIONS) + line[column_num + 1:])
-    
-    return return_arr
+    return distance_in_x + distance_in_y
 
-# Add in the extra rows
+def add_extra_rows(expansion_array: list[list[int]], row_num: int) -> list[list[int]]:
+    '''Adds an expansion coordanite to the passed in array.
+    Returns the updated expansion array'''
+    
+    expansion_array[1].append(row_num)
+    
+    return expansion_array
+    
+def add_extra_columns(expansion_array: list[list[int]], row_num: int) -> list[list[int]]:
+    '''Adds an expansion coordanite to the passed in array.
+    Returns the updated expansion array'''
+    
+    expansion_array[0].append(row_num)
+    
+    return expansion_array
+
+# Add to the expansions array
+expansions = [[], []]
+
 for i in reversed(range(len(lines))):
     if len(set(lines[i])) == 1:
-        lines = add_extra_rows(lines, i)
-
-# Add extra columns
+        expansions = add_extra_rows(expansions, i)
+        
 for i in reversed(range(len(lines[0]))):
     empty = True
     for j in range(len(lines)):
@@ -55,8 +64,9 @@ for i in reversed(range(len(lines[0]))):
             empty = False
     
     if empty:
-        lines = add_extra_columns(lines, i)
+        expansions = add_extra_columns(expansions, i)
 
+print(expansions)
 
 # Get all star locations
 galaxy_coords = []
@@ -77,16 +87,6 @@ for i, galaxy in enumerate(galaxy_coords):
     # Go through each other galaxy and add the distances to the array
     for other in other_galaxies:
         current_coords = galaxy
-        num_steps = 0
-        # Find the distance
-        while True:
-            current_coords = calculate_next_coord(current_coords, other)
-            num_steps += 1
-            
-            if current_coords == other:
-                distances.append(num_steps)
-                break
+        distances.append(calculate_distance(expansions, galaxy, other))
     
-
-
 print(sum(distances))
