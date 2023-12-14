01fa75e788b3c47d55ab340fe6b4ec29c3dd3f72
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2023-12-14 13:55:11 -0500

    Working on day 12

--- day-12/day-12-1.py
+++ day-12/day-12-1.py
@@ -1,3 +1,8 @@
 # Use list comprehension to open the file and parse through
 day = 12
 lines = [line.strip() for line in open("day-" + str(day) + "/sample.txt", "r")]
+
+def get_combos_for_line(line: str, nums: list[int]) -> int:
+    '''Returns the number of combinations possible for this line'''
+    
+    
